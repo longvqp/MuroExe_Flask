73596e51afadc7755fc5aaa1c80180f019ab318d
Long Vu <vuquangphilong@gmail.com>
2022-09-22 13:09:57 +0700

    Finished Flask and Page Header

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
 from flask import Flask, render_template
-from flask_bootstrap import Bootstrap4
+from flask_bootstrap import Bootstrap5
 
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-bootstrap = Bootstrap4(app)
+bootstrap = Bootstrap5(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
