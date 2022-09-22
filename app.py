236cad00b0a21aa92734163b81e35e6c581581bd
Long Vu <vuquangphilong@gmail.com>
2022-09-22 12:46:11 +0700

    Inital Flask

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap4
+
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__)
+bootstrap = Bootstrap4(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+   return render_template('index.html')
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
