31b9bb38590c55d0eab0e4227610d920e851c955
Long Vũ <vuquangphilong@gmail.com>
2022-10-24 22:45:15 +0700

    Update Git Ignore

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,13 +1,20 @@
 from flask import render_template
 from .forms import RegistrationForm
 from . import auth
-
+from ..models import User
 @auth.route('/login', methods=['GET','SET'])
 def login():
     return render_template('auth/login.html')
 
 
-@auth.route('/register', methods=['GET','SET'])
+@auth.route('/register', methods=['GET','POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data,
+                    username = form.username.data,
+                    password = form.password.data)
+        print(user.email)
+        
+        
     return render_template('auth/register.html',form = form)
